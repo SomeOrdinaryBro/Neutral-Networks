d7bd752657b7caa6d448a4050c9f73a027d7c29b
Sajid <sajid@duck.com>
2024-03-17 11:11:44 +0530

    Made improvements to controls & the car (Added w,a,s,d)

--- controls.js
+++ controls.js
@@ -1,45 +1,53 @@
-class Controls{
-    constructor(){
-        this.forward=false;
-        this.left=false;
-        this.right=false;
-        this.reverse=false;
+class Controls {
+    constructor() {
+        this.forward = false;
+        this.left = false;
+        this.right = false;
+        this.reverse = false;
 
         this.#addKeyboardListeners();
     }
 
-    #addKeyboardListeners(){
-        document.onkeydown=(event)=>{
-            switch(event.key){
+    #addKeyboardListeners() {
+        document.onkeydown = (event) => {
+            switch (event.key) {
                 case "ArrowLeft":
-                    this.left=true;
+                case "a":
+                    this.left = true;
                     break;
                 case "ArrowRight":
-                    this.right=true;
+                case "d":
+                    this.right = true;
                     break;
                 case "ArrowUp":
-                    this.forward=true;
+                case "w":
+                    this.forward = true;
                     break;
                 case "ArrowDown":
-                    this.reverse=true;
+                case "s":
+                    this.reverse = true;
                     break;
             }
-        }
-        document.onkeyup=(event)=>{
-            switch(event.key){
+        };
+        document.onkeyup = (event) => {
+            switch (event.key) {
                 case "ArrowLeft":
-                    this.left=false;
+                case "a":
+                    this.left = false;
                     break;
                 case "ArrowRight":
-                    this.right=false;
+                case "d":
+                    this.right = false;
                     break;
                 case "ArrowUp":
-                    this.forward=false;
+                case "w":
+                    this.forward = false;
                     break;
                 case "ArrowDown":
-                    this.reverse=false;
+                case "s":
+                    this.reverse = false;
                     break;
             }
-        }
+        };
     }
 }
